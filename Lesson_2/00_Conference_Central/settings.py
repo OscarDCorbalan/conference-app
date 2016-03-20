9175f68756ee7deca9f081e9ce069723c614c3d1
Oscar Dominguez <oscardc@gmx.com>
2016-03-20 19:47:52 +0100

    chore: Setup app data

--- Lesson_2/00_Conference_Central/settings.py
+++ Lesson_2/00_Conference_Central/settings.py
@@ -12,5 +12,5 @@ created/forked from conference.py by wesc on 2014 may 24
 
 # Replace the following lines with client IDs obtained from the APIs
 # Console or Cloud Console.
-WEB_CLIENT_ID = 'replace with Web client ID'
+WEB_CLIENT_ID = '338700913536-8i10e5omc02mpt4elg2hpmijqoiecp05.apps.googleusercontent.com'
 
--- Lesson_2/00_Conference_Central/static/js/app.js
+++ Lesson_2/00_Conference_Central/static/js/app.js
@@ -86,7 +86,7 @@ app.constant('HTTP_ERRORS', {
  */
 app.factory('oauth2Provider', function ($modal) {
     var oauth2Provider = {
-        CLIENT_ID: 'web-client-id',
+        CLIENT_ID: '338700913536-8i10e5omc02mpt4elg2hpmijqoiecp05.apps.googleusercontent.com',
         SCOPES: 'email profile',
         signedIn: false
     }
