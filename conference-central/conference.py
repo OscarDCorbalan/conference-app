fcc678e06d41ad9741b4edebd032c0c975441127
Oscar Dominguez <oscardc@gmx.com>
2016-03-24 11:55:34 +0100

    fix: Add constants for default conference form fields

--- conference-central/conference.py
+++ conference-central/conference.py
@@ -30,6 +30,12 @@ from utils import get_user_id
 
 EMAIL_SCOPE = endpoints.EMAIL_SCOPE
 API_EXPLORER_CLIENT_ID = endpoints.API_EXPLORER_CLIENT_ID
+DEFAULTS = {
+    "city": "Default City",
+    "maxAttendees": 0,
+    "seatsAvailable": 0,
+    "topics": [ "Default", "Topic" ],
+}
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
